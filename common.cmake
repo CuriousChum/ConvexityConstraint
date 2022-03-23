06bb6187e686d784a17c90ea4f72e555cf3c9f0f
Jean-Marie Mirebeau <jm.mirebeau@gmail.com>
2022-03-23 15:51:13 +0100

    Prepare for PC version

--- common.cmake
+++ common.cmake
@@ -1,5 +1,6 @@
 # --- !! TO USER : Change these two paths !! ---
 
+# On JMM's macbook
 SET(Conda_ConvexityConstraint_Env_Dir "/Users/mirebeau/opt/miniconda3/envs/ConvexityConstraint/")
 SET(HamiltonFastMarching_Source_Dir "/Users/mirebeau/Dropbox/Programmes/Github/HamiltonFastMarching")
 
@@ -7,8 +8,20 @@ SET(HamiltonFastMarching_Source_Dir "/Users/mirebeau/Dropbox/Programmes/Github/H
 # - A ConvexityConstraint conda environment, containing the cgal, nlopt, and eigen packages, using the provided yaml file. This can be done with the following command : conda env create -f ConvexityConstraint.yaml
 # - The HamiltonFastMarching repository contents, which can be been downloaded at : https://github.com/Mirebeau/HamiltonFastMarching
 
-# ------------------------------------------
+# ------------------ PC version (not working yet) ------------------------
 
+# On JMM's PC
+#SET(Conda_ConvexityConstraint_Env_Dir "C:/Users/jmmir/miniconda3/envs/ConvexityConstraint")
+#SET(HamiltonFastMarching_Source_Dir "C:/Users/jmmir/Documents/GitHub/HamiltonFastMarching")
+
+#set(CMAKE_PREFIX_PATH ${CondaDir} ${CondaDir}/Library/lib/CGAL)
+
+#Set(CgalLib  "${CondaDir}/Library/lib/CGAL-vc140-mt-4.9.1.lib")   # CGAL
+#Set(NloptLib "${CondaDir}/Library/lib/nlopt.lib")  # NLOPT
+#Set(GmpLib   "${CondaDir}/Library/lib/gmp.lib")    # GMP
+
+
+# ------------------- Mac version ----------------------
 
 # General setup
 set(CondaDir ${Conda_ConvexityConstraint_Env_Dir})
